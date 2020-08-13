f1883bded46d416bc528295c89e34c4645c5707d
pjupju <tkachenkoyuriy12@gmail.com>
2020-08-13 16:46:17 +0300

    some fixes

--- src/components/404.js
+++ src/components/404.js
@@ -3,7 +3,7 @@ import './../css/404.css';
 
 function About() {
     return (
-        <div className="noMatchFlex-center noMatchPosition-r noMatchFull-height">
+        <div className="noMatchFlex noMatchPosition noMatchFullHeight">
         <div className="noMatchCode">404</div>
         <div className="noMatchMessage">NOT FOUND</div>
         </div>
